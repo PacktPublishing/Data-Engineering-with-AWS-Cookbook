c00786424607f8bc68d58a1af181e916962e3245
Gonzalo Herreros <gherrero@redhat.com>
2024-08-30 16:27:42 +0100

    Added blank line for clarity

--- Chapter05/Recipe1/testEMRServerless.py
+++ Chapter05/Recipe1/testEMRServerless.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+
 spark = (SparkSession.builder.config(
          "hive.metastore.client.factory.class",
          "com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory")
