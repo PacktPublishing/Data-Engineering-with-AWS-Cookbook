391827bcbd6a4ee2f310b497a7b64566570e928e
VKhan <vaquar.khan@gmail.com>
2024-10-10 16:34:31 -0500

    Create dynamodb-import-glue-job.py

---
+++ Chapter13/dynamodb-import-glue-job.py
@@ -0,0 +1,13 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+glueContext = GlueContext(SparkContext.getOrCreate())
+dynamic_frame = glueContext.create_dynamic_frame.from_catalog(database = "your-database-name", table_name = "your-table-name")
+
+# Transformation logic here
+
+glueContext.write_dynamic_frame.from_options(frame = dynamic_frame, connection_type = "dynamodb", connection_options = {"dynamodb.output.tableName": "your-dynamodb-table-name"})
