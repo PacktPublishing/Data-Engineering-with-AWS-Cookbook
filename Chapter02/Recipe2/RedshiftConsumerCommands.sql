05a220d1b1d11e56ef158bcef85c4d3fe4c53bc6
Huda Nofal <himosbah@amazon.com>
2024-12-01 12:41:07 +0100

    Updating Ch2-Recipe2

--- Chapter02/Recipe2/RedshiftConsumerCommands.sql
+++ Chapter02/Recipe2/RedshiftConsumerCommands.sql
@@ -2,9 +2,6 @@ CREATE DATABASE consumer_database_name
 FROM DATASHARE datashare_name OF ACCOUNT 'producer_account_ID'
 NAMESPACE 'producer_cluster_namespace';
 
-CREATE EXTERNAL SCHEMA consumer_schema_name 
-FROM redshift DATABASE consumer_database_name
-SCHEMA producer_schema_name;
 
 SELECT * FROM
 consumer_database_name.consumer_schema_name.table_name;
